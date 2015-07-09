b56423606fd70a83ff662dd6c09763e5cf5b9f3c
Kevin Pan <pankevin128@gmail.com>
2015-07-09 16:26:33 +0200

    init
    init

---
+++ Samples/GccApplication1/GccApplication1/GccApplication1.c
@@ -0,0 +1,17 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 4/10/2015 8:55:54 AM
+ *  Author: kpan
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
